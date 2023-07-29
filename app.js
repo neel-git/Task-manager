7e35bd3a08222f287a198b46481a5f1d6c77a8f0
Neelkamal <neelkamalsinghthegisb@gmail.com>
2023-07-30 01:19:47 +0530

    custom error-handler added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 //middleware
 app.use(express.static("./public"));
@@ -10,8 +11,9 @@ app.use(express.json());
 //routes
 
 app.use("/api/v1/tasks", tasks);
+app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
